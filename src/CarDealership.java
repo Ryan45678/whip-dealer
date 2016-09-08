0fd277491df100982b5d2c3a192a7401c78866f2
Ryan45678 <ryan.askew@hotmail.com>
2016-09-08 14:26:11 -0500

    Update CarDealership.java

--- src/CarDealership.java
+++ src/CarDealership.java
@@ -1,11 +1,36 @@
+package src;
 import src.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CarDealership
 {
-	public static void main(String[] args)
-	{
-		ArrayList<Car> cars = new ArrayList<Car>();
-		System.out.println("Car Dealership");
-	}
+    public static void main(String[] args)
+    {
+        ArrayList<Car> cars = new ArrayList<Car>();
+        // read in from cars.txt, or create it
+        
+        System.out.println("Car Dealership");
+        
+        // initialize the chosen option to 0
+        int option = 0;
+        Scanner scan = new Scanner(System.in);
+        
+        do
+        {
+            switch (option)
+            {
+                case 0: //function call
+                        break;
+                
+            }
+            System.out.println("Select an option by entering a number from 1-6.\n");
+            System.out.println("Press '0' to display the list of options.");
+            
+            option = scan.nextInt();
+            
+        }while(option != 6);
+        
+        scan.close();
+    }
 }
