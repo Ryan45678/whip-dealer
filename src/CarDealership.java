f7cb1790cc4173967c70bc5dfefb7ed614667f39
Ryan45678 <ryan.askew@hotmail.com>
2016-09-08 18:56:28 -0500

    Update CarDealership.java
    Implemented:
    
    Full menu functionality
    Full input validation for menu options
    Ends program when option 6 is selected and the menu loop is exited - list saving method does not need to include closing the program
    
    
    
    Missing methods for: 
    
    - load or create car list
    - show menu, show records, add car, remove car, search by VIN, search by price range 
    - save list to file

--- src/CarDealership.java
+++ src/CarDealership.java
@@ -1,6 +1,7 @@
 package src;
 
 import src.*;
+import src.Car;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,26 +14,26 @@ public class CarDealership
         // read in from cars.txt, or create it
         
         // initialize the chosen option to 0
-        int option = 0;
+        char option = '0';
         
         do
         {
             Scanner scan = new Scanner(System.in);
             switch (option)
             {
-                case 0: // show menu
+                case '0': // show menu
                         break;
-                case 1: // show records
+                case '1': // show records
                         break;
-                case 2: // add new car
+                case '2': // add new car
                         break;
-                case 3: // delete car
+                case '3': // delete car
                         break;
-                case 4: // search for car by vin
+                case '4': // search for car by vin
                         break;
-                case 5: // search for cars in price range
+                case '5': // search for cars in price range
                         break;
-                case 6: // exit program
+                case '6': // exit program
                         break;
             }
             
@@ -41,20 +42,33 @@ public class CarDealership
             System.out.println("Press '0' to display the list of options.");
             
             // get the user's choice of option
-            option = scan.nextInt();
+            String temp1 = scan.nextLine();
+            if (temp1.length() > 0)
+                option = temp1.charAt(0);
+            else
+                temp1 = " ";
             
             // input validation
-            while (option != 0 && option != 2 && option != 3 && option != 4 && option != 5 &&
-                   option != 6)
+            while ((option != '0' && option != '1' && option != '2' && option != '3' && 
+            option != '4' && option != '5' && option != '6') || temp1 == " ")
             {
                 System.out.println("Invalid input!\n\n");
                 System.out.println("Select an option by entering a number from 1-6.\n");
                 System.out.println("Press '0' to display the list of options.");
-                option = scan.nextInt();
+                temp1 = scan.nextLine();
+                
+                if (temp1.length() > 0)
+                    option = temp1.charAt(0);
+                else
+                    temp1 = " ";
+                
                 
             }
             
-        }while(option != 6); // keep the program running until user selects option 6
+            
+        }while(option != '6'); // keep the program running until user selects option 6
         
+        System.out.println("Program Terminated");
+        System.exit(0);
     }
 }
